e6e5a3d1ede068dedcc639d90241bbf0ee19c484
manthanshahgit-ui <manthanshahgit@gmail.com>
2026-08-17 21:39:11 +0530

    Solve SQL Name of Employees - Question 14

---
+++ question_14.sql
@@ -0,0 +1,19 @@
+-- Question Description
+
+-- This HackerRank problem asks you to:
+
+-- Retrieve the name column from the Employee table.
+-- Display only the employee names.
+-- Sort the names in alphabetical order (ascending).
+-- No filtering or conditions are required.
+-- SQL Query
+link="https://www.hackerrank.com/challenges/name-of-employees/problem?"
+SELECT name
+FROM Employee
+ORDER BY name ASC;
+
+-- ASC means ascending order, so the names will be displayed alphabetically.
+
+-- Git Commit Message
+
+-- For HackerRank Name of Employees:
