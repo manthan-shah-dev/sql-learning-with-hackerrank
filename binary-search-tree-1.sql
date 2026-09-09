bbd8594aad3be494ac6420be7d1c68c699ca7825
MANTHAN SHAH <manthanshahgit@gmail.com>
2026-09-09 23:44:14 +0530

    Update comments for node types in binary search tree
    Clarified comments regarding node types in the BST.

--- binary-search-tree-1.sql
+++ binary-search-tree-1.sql
@@ -1,7 +1,7 @@
 -- # Binary Search Tree 1
 
 -- This SQL query identifies the type of each node in the BST.
---
+
 -- The node can be one of three types:
 -- Root:
 -- If the node has no parent, then P is NULL.
